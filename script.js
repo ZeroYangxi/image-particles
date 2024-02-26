568c96eb3b8dd3bb47ddd1ff298e51ed2f05db3a
ZeroYangxi <riveryangxi@gmail.com>
2024-02-26 13:41:19 -0500

    implement warp button and style the button

--- script.js
+++ script.js
@@ -34,6 +34,11 @@ window.addEventListener("load", function () {
       this.x += (this.originX - this.x) * this.ease;
       this.y += (this.originY - this.y) * this.ease;
     }
+    warp() {
+      this.x = Math.random() * this.effect.width;
+      this.y = Math.random() * this.effect.height;
+      this.ease = 0.01;
+    }
   }
 
   // handle entire effect
@@ -82,6 +87,9 @@ window.addEventListener("load", function () {
     update() {
       this.particlesArray.forEach((particle) => particle.update());
     }
+    warp() {
+      this.particlesArray.forEach((particle) => particle.warp());
+    }
   }
   const effect = new Effect(canvas.width, canvas.height);
   effect.init(context);
@@ -94,4 +102,11 @@ window.addEventListener("load", function () {
     requestAnimationFrame(animate);
   }
   animate();
+
+  // warp button
+  const warpButton = document.getElementById("warpButton");
+  warpButton.addEventListener("click", function () {
+    effect.warp();
+    console.log("clicked");
+  });
 });
